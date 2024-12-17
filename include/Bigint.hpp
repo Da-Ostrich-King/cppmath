db28c6915d485e5b5504ee3a3d8ac879d5c86fb4
liam waaga <liamgames505@gmail.com>
2024-12-16 18:51:58 -0800

    some additions for gruyere::math::Bigint

--- include/Bigint.hpp
+++ include/Bigint.hpp
@@ -2,25 +2,25 @@
 #include <ostream>
 #include <vector>
 #include <iostream>
+namespace gruyere {
+    namespace math {
+        template< int precision >
+        class Bigint;
 
-namespace math {
-    template< int precision >
-    class Bigint;
+        template< int precision >
+        std::ostream& operator<<( std::ostream &out, Bigint< precision > const &data );
 
-    template< int precision >
-    std::ostream& operator<<( std::ostream &out, Bigint< precision > const &data );
+        template< int precision > // precision in bytes
+        class Bigint {
+        public:
+            Bigint();
+            
+        private:
+            std::vector< uint8_t > data;
+            bool sign;
 
-
-
-
-    template< int precision > // precision in bytes, must be a multiple of 64
-    class Bigint {
-    public:
-        Bigint();
-        
-    private:
-        std::vector< uint64_t > data;
-        bool sign;
-        friend std::ostream& operator<< <>( std::ostream &out, Bigint< precision > const &data );
-    };
+            template< int size >
+            friend std::ostream& operator<<( std::ostream &out, Bigint< precision > const &data );
+        };
+    }
 }
--- src/Bigint.cpp
+++ src/Bigint.cpp
@@ -1,5 +1,14 @@
 #include "../include/Bigint.hpp"
 
-template< int size > math::Bigint< size >::Bigint() {
-    data.resize( size / 4 );
+
+template< int size > gruyere::math::Bigint< size >::Bigint() {
+    data.resize( size );
+}
+
+template< int precision >
+std::ostream& operator<< ( std::ostream& out, gruyere::math::Bigint< precision > const &data) {
+    for ( int i = 0; i < data.length(); i++ ) {
+        out << ( int ) data[ i ];
+    }
+    return out;
 }
